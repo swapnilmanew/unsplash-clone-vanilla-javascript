43a1247532cf474a9c42d5f96f91764cf9723a92
swapnilmanew <swapnilmanew@gmail.com>
2022-04-17 18:55:39 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const row = document.getElementById('row');
+const response = axios.get('https://picsum.photos/v2/list').then((res) =>
+  res.data.forEach((image, index) => {
+    row.innerHTML += `
+<div class="col-12 col-sm-6 col-md-4 mx-auto my-4">
+<div class="card">
+  <img
+    src="${image.download_url}"
+    alt=""
+    class="card-img-top"
+  />
+  <div class="card-body text-center">
+    <p>${image.author}</p>
+    <a href="${image.download_url}" class="btn btn-sm btn-primary">Download</a>
+  </div>
+</div>
+</div>
+`;
+  })
+);
